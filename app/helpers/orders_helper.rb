1e029c21f11b898a68518392120220b055f6bee5
Cyril ViXP <cyrilvixp@gmail.com>
2015-11-12 14:51:25 +0200

    Payment types translated

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,12 @@
 module OrdersHelper
+
+	def pay_types_translate
+	# returns the array of payment types in form of: value: DB-value, text: text value 
+		result = []
+		PaymentType.names.each do |type|
+			result << { :value => type, :text => I18n::t(type.downcase.gsub(/\s+/, "")) }
+		end
+		result
+	end
+
 end
